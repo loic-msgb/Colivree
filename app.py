719f4261f0206b12d4d62cdd73e44dd5081465f9
loic-msgb <christophermissigbeto@gmail.com>
2024-08-27 17:40:58 +0200

    login fonctionne

--- app.py
+++ app.py
@@ -1,49 +1,76 @@
-from flask import Flask, render_template, request
-from models import db, Trip  # Importation de db et Trip depuis models.py
+from flask import Flask, render_template, request, redirect, url_for, flash, session
+from flask_migrate import Migrate
+
+from models import db, Trip, User  # Importation de db, Trip et User depuis models.py
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
 # Configuration de la base de données SQLite
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///mydatabase.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SECRET_KEY'] ='mysecretkey'
 # Initialisation de l'extension SQLAlchemy avec l'application Flask
 db.init_app(app)
+
+migrate = Migrate(app, db)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/add_travel', methods=['POST'])
-def ajouter_voyage():
-    # Récupérer les données du formulaire
-    departure = request.form.get('departure')
-    arrival = request.form.get('arrival')
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
 
-    departure_date = request.form.get('departure-date')
-    departure_date = datetime.strptime(departure_date, '%Y-%m-%d').date()
-    arrival_date = request.form.get('arrival-date')
-    arrival_date = datetime.strptime(arrival_date, '%Y-%m-%d').date()
+        # Vérifier si l'utilisateur ou l'email existe déjà
+        user = User.query.filter_by(username=username).first()
+        email_exists = User.query.filter_by(email=email).first()
 
-    weight = float(request.form['weight'])
+        if user:
+            flash('Ce nom d\'utilisateur est déjà pris.')
+            return redirect(url_for('signup'))
 
-    # Créer un nouvel objet Trip
-    trip = Trip(departure_location=departure, arrival_location=arrival, departure_date=departure_date, arrival_date=arrival_date, weight=weight)
+        if email_exists:
+            flash('Cet email est déjà utilisé.')
+            return redirect(url_for('signup'))
 
-    # Ajouter l'objet à la base de données
-    db.session.add(trip)
-    db.session.commit()
-
-    # Traiter les données
-    print(departure, arrival, departure_date, arrival_date, weight)
-
-    trips = Trip.query.all()  # Récupère tous les voyages de la base de données
-    return render_template('trips.html', trips=trips)
-
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
+        # Ajouter un nouvel utilisateur
+        new_user = User(username=username, email=email, password=generate_password_hash(password, method='pbkdf2:sha256'))
+        db.session.add(new_user)
         db.session.commit()
-        
+
+        return redirect(url_for('index'))
+    else:
+        return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        identifier = request.form.get('identifier')  # Le nom ou l'email peut être saisi
+        password = request.form.get('password')
+
+        # Rechercher l'utilisateur soit par email, soit par nom d'utilisateur
+        user = User.query.filter((User.email == identifier) | (User.username == identifier)).first()
+
+        # Vérifier les informations d'identification
+        if not user or not check_password_hash(user.password, password):
+            flash('Veuillez vérifier vos identifiants et réessayer.')
+            return redirect(url_for('login'))
+
+        # Stocker les informations de session
+        session['user_id'] = user.id
+        session['username'] = user.username
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html')
+
+    
+
+
+if __name__ == '__main__':        
     app.run(debug=True)
--- models.py
+++ models.py
@@ -4,13 +4,21 @@ from datetime import datetime
 # Initialise l'extension SQLAlchemy (db est une instance de SQLAlchemy)
 db = SQLAlchemy()
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+    
+
 class Trip(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     departure_date = db.Column(db.DateTime, nullable=False)
     arrival_date = db.Column(db.DateTime, nullable=False)
     departure_location = db.Column(db.String(255), nullable=False)
     arrival_location = db.Column(db.String(255), nullable=False)
-    weight = db.Column(db.Float, nullable=False)  # Le poids disponible en kg
+    available_weight = db.Column(db.Float, nullable=False)  # Le poids disponible en kg
 
     def __repr__(self):
         return f'<Trip from {self.departure_location} to {self.arrival_location}>'
